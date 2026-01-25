07d7df0272b5106f2cb4cba3faef1e3e8df96687
24BAI71026-Dibyanshu <krish9805438835@gmail.com>
2026-01-26 00:17:52 +0530

    Update librarian.sql

--- Experiment-1/Experiment_1/librarian.sql
+++ Experiment-1/Experiment_1/librarian.sql
@@ -2,7 +2,7 @@ SELECT * FROM books;
 select * from book_issue;
 select * from LIBRARY_VISITOR_USER;
 
-INSERT INTO books VALUES(110,'ABCD','CLANS',2);
+INSERT INTO books VALUES(120,'arya','xya',2);
 INSERT INTO books VALUES(150,'THE LORD','HRM',7);
 
 DELETE FROM books
